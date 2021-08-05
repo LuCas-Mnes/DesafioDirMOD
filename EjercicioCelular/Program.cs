298a023813a3fd22c6e388d554b53b4a278ae128
Lucas Manuel Ruiz <69923008+LuCas-Mnes@users.noreply.github.com>
2021-08-04 23:42:30 -0300

    Add files via upload

---
+++ EjercicioCelular/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EjercicioCelular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcesarTexto procesadorTexto = new ProcesarTexto();
+            Console.WriteLine("Por Favor Ingrese el texto: ");
+            string texto = Console.ReadLine();
+            string resultado = procesadorTexto.teclas(texto);
+            Console.WriteLine("La Combinación Ingresada es: "+resultado);
+        }
+    }
+}
